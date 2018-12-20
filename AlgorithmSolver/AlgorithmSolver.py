9cdda53823ec4f03664c8a9c9dbd7d8cd02669a5
DESKTOP-USEP09U\sagiv <sagivmap@gmail.com>
2018-12-20 11:41:05 +0200

    alex change commit

---
+++ AlgorithmSolver/AlgorithmSolver.py
@@ -0,0 +1,86 @@
+from Node import Node
+from Edge import Edge
+import random, string, csv
+
+VOWELS = "aeiou"
+CONSONANTS = "".join(set(string.ascii_lowercase) - set(VOWELS))
+currUser = "Sagiv"
+
+class AlgorithmSolver:
+
+    def __init__(self, path_of_csv_file):
+        self.path_of_csv_file = path_of_csv_file
+        self.nodes = []
+        self.edges = []
+        self.first_circle_nodes = []
+        self.second_circle_nodes = []
+
+    def decode_name(self):
+
+        return "tmp"
+
+    def makearrayfromstring(self, string):
+        ans = []
+        if len(string) == 2:
+            return ans
+
+        tmp = string[1:-1].split(', ')
+        for x in tmp:
+            ans.append(x[1:-1] + "")
+        return ans
+
+    def create_edges(self):
+        ans = []
+
+        for node in self.nodes:
+            tmp = list(set(node.cf))
+            if len(tmp) == 1:
+                ans.append(Edge(currUser if tmp[0] == '0' else tmp[0] , node.idd))
+            else:
+                for user in tmp:
+                    ans.append(Edge(user, node.idd))
+
+        return list(set(ans))
+
+    def create_nodes_and_edges(self):
+        with open(self.path_of_csv_file, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+
+            for row in csv_reader:
+                name = row["Name"][2:-1]
+                if not name[0].isalpha():
+                    name = self.decode_name()
+
+                cf = self.makearrayfromstring(row["CF"])
+
+                node = Node(row["ID"], name, row["TF"], row["MF"], row["AUA"], row["FD"], cf)
+
+                self.nodes.append(node)
+
+                if '0' in cf:
+                    self.first_circle_nodes.append(node)
+                else:
+                    self.second_circle_nodes.append(node)
+
+        self.edges = self.create_edges()  # after that , we have all nodes and edges
+
+        # sort of a logger
+        with open('./files/firstCircle.txt', 'w') as f:
+            for item in self.first_circle_nodes:
+                f.write("%s\n" % item)
+        with open('./files/secondCircle.txt', 'w') as f:
+            for item in self.second_circle_nodes:
+                f.write("%s\n" % item)
+        with open('./files/allEdges.txt', 'w') as f:
+            for edge in self.edges:
+                f.write("%s\n" % edge)
+
+    def generate(self):
+        self.create_nodes_and_edges()
+        self.calculate_weights()
+
+
+if __name__ == "__main__":
+    algSolv = AlgorithmSolver("sagiv_data.csv")
+    algSolv.generate()
+    pass

e0886fffe81a8b3fe29d14ac9142a78422382aad
DESKTOP-USEP09U\sagiv <sagivmap@gmail.com>
2019-03-12 10:10:18 +0200

    d3 try

--- FlaskTry/mainServer.py
+++ FlaskTry/mainServer.py
@@ -37,8 +37,7 @@ def handle_posts():
             return upload_file()
 
         elif request.form["button"]=="MoveToManuallyAddPage":
-            print ('aaaaaa')
-            return render_template('index.html')
+            return render_template('addManuallyPage.html')
 
 
 if __name__ == '__main__':

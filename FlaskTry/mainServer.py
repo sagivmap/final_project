295128791d4d79c41f39281a937bbe70eaffe921
DESKTOP-USEP09U\sagiv <sagivmap@gmail.com>
2019-03-19 10:42:45 +0200

    d3 try to delete cache 3

--- FlaskTry/mainServer.py
+++ FlaskTry/mainServer.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
-from flask import make_response
-from functools import wraps, update_wrapper
-from datetime import datetime
-from Crawler import FBCrawler
+from flask import Flask, render_template, request, redirect
+
 import os
+import json
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
+PATH_TO_JSON = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static','graphFile.json')
+
+ID_FOR_NODE = 1
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
+app.config['PATH_TO_JSON'] = PATH_TO_JSON
+app.config['ID_FOR_NODE'] = 1
 @app.route('/')
 def main():
     return render_template('index.html')
@@ -30,6 +32,28 @@ def upload_file():
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
         return render_template('index.html')
 
+
+def add_new_node():
+    with open(app.config['PATH_TO_JSON']) as f:
+        json_data = json.load(f)
+
+    Node_to_add = {}
+    Node_to_add['id'] = app.config['ID_FOR_NODE']
+    app.config['ID_FOR_NODE'] += 1
+    Node_to_add['name'] = request.form['NodeName']
+    Node_to_add['TF'] = int(request.form['TF'])
+    Node_to_add['AUA'] = int(request.form['AUA'])
+    Node_to_add['CF'] = [int(x) for x in request.form['CF'].split(',')]
+    Node_to_add['MF'] = [int(x) for x in request.form['MF'].split(',')]
+    Node_to_add['FD'] = [int(x) for x in request.form['FD'].split(',')]
+
+    json_data['nodes'].append(Node_to_add)
+
+    with open(app.config['PATH_TO_JSON'], "w") as jsonFile:
+        json.dump(json_data, jsonFile, sort_keys=True, indent=4)
+
+    return render_template('addManuallyPage.html')
+
 @app.route('/', methods=['POST'])
 def handle_posts():
     if request.method == 'POST':
@@ -41,6 +65,8 @@ def handle_posts():
 
         elif request.form["button"]=="MoveToManuallyAddPage":
             return render_template('addManuallyPage.html')
+        elif request.form["button"]=="AddNode":
+            return add_new_node()
 
 
 

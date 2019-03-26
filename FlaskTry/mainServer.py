c325a3e3f8c750cbae3a17638b2583b4d9f7d3a4
alexchinyan <alexan.c09@gmail.com>
2019-03-26 16:40:31 +0200

    - added button for crawl twitter - added button for upload twitter csv - CSV to Json - showStaticGraph

--- FlaskTry/mainServer.py
+++ FlaskTry/mainServer.py
@@ -23,15 +23,43 @@ def crawl_facebook():
     print(email, password)
     return render_template('index.html')
 
+def crawl_twitter():
+    nick_name = request.form['twitterName']
+    print(nick_name)
+    return render_template('index.html')
+
 def upload_file():
     # check if the post request has the file part
     if 'file' not in request.files:
         return redirect(request.url)
     file = request.files['file']
     if file:
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        return render_template('index.html')
+        path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(path)
+        import csv
+        import json
+        # csv to Json
+        csvfile = open(path, mode='r')
+        jsonfile = open('file.json', 'w')
+        fieldnames = ("ID","Name","TF","MF","AUA","FD","CF")
+        reader = csv.DictReader(csvfile, fieldnames)
+        next(reader) #the first row is fieldnames
+        nodes = []
+        links = []
 
+        for row in reader:
+            CF = eval(row['CF'])
+            ID = row['ID']
+            nodes.append({"Id":ID, "name":row['Name'], "TF":row['TF'], "AUA":row['AUA'], "CF":CF, "MF":row['MF'], "FD":row['FD']})
+            for idd in CF:
+                links.append({"Source":ID,"Target":idd})
+
+        json.dump({'nodes': nodes,'links': links},jsonfile)
+
+        csvfile.close()
+        jsonfile.close()
+
+        return render_template('showGraph.html')
 
 def add_new_node():
     with open(app.config['PATH_TO_JSON']) as f:
@@ -62,7 +90,8 @@ def handle_posts():
 
         elif request.form["button"]=="Upload":
             return upload_file()
-
+        elif request.form["button"]=="CrawlTwitter":
+            return crawl_twitter()
         elif request.form["button"]=="MoveToManuallyAddPage":
             return render_template('addManuallyPage.html')
         elif request.form["button"]=="AddNode":

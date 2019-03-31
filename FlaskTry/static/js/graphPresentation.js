b07c1d324b213e776f1357565ef6540a8b3656fa
DESKTOP-USEP09U\sagiv <sagivmap@gmail.com>
2019-03-31 16:18:40 +0300

    node separate levels

--- FlaskTry/static/js/graphPresentation.js
+++ FlaskTry/static/js/graphPresentation.js
@@ -1,249 +1,217 @@
-function myGraph(el) {
-    var id_count = 1;
-    var tf_barrier = 100;
-    var aua_barrier = 365;
-    var mf_barrier = 20;
-    var fd_barrier = 12;
-    var msp = 0.5;
+var width = $("#my_dataviz").innerWidth(),
+    height = $("#my_dataviz").innerHeight(),
+    id_count = 1,
+    tf_barrier = 100,
+    aua_barrier = 365,
+    mf_barrier = 20,
+    fd_barrier = 12,
+    msp = 0.5,
+    nodes = [{ "id": 0, "name": "Ego Node", "TF": "", "AUA": "", "CF": [], "MF": [], "FD": [], "Weight": -1, "TSP": -1, "level": 0 }],
+    links = [],
+    getXloc = d3.scalePoint().domain([0, 1, 2]).range([100, width - 100]);
 
-    // Initialise the graph object
-    var graph = this.graph = {
-        "nodes":[{"Id":0, "name":"Ego Node", "TF":"", "AUA":"", "CF":[], "MF":[], "FD":[], "Weight":-1, "TSP":-1}],
-        "links":[]
-    };
+var simulation = d3.forceSimulation(nodes)
+    .force('x', d3.forceX((d) => getXloc(d.level)).strength(4))
+    .force('collide', d3.forceCollide(10))
+    .force('charge', d3.forceManyBody().strength(0))
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('link', d3.forceLink().links(links))
+    .on('tick', ticked);
 
-    var calc_node_weight = function (TF, AUA){
-        var c_TF;
-        if (TF > tf_barrier){
-            c_TF = 1;
-        }else{
-            c_TF = TF/tf_barrier;
-        }
-        var c_AUA;
-        if (AUA > aua_barrier){
-            c_AUA = 1;
-        }else{
-            c_AUA = AUA/aua_barrier;
-        }
+//drag handler
+//d is the node
+function drag_start(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
 
-        return (c_TF + c_AUA)/2;
-    }
+function drag_drag(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
 
-    // Add and remove elements on the graph object
-    this.addNode = function (Name, TF, AUA, CF, MF, FD) {
-        var node_weight = calc_node_weight(TF, AUA);
-        graph["nodes"].push({"Id":id_count, "name":Name, "TF":TF, "AUA":AUA, "CF":CF, "MF":MF, "FD":FD,
-                            "Weight":node_weight, "TSP":-1});
+function drag_end(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+}
 
-    }
+var drag_handler = d3.drag()
+    .on("start", drag_start)
+    .on("drag", drag_drag)
+    .on("end", drag_end);
 
-    this.removeNode = function (name) {
-        graph["nodes"] = _.filter(graph["nodes"], function(node) {return (node["name"] != name)});
-        graph["links"] = _.filter(graph["links"], function(link) {return ((link["source"]["name"] != name)&&(link["target"]["name"] != name))});
-        update();
-    }
+// add 1 to id_count
+function increase_id_count(){
+    id_count++;
+}
+// node and link weight calculator
+var calc_node_weight = function (TF, AUA) {
+    var c_TF, c_AUA;
+    if (TF > tf_barrier) { c_TF = 1; }
+    else { c_TF = TF / tf_barrier; }
+    if (AUA > aua_barrier) { c_AUA = 1; }
+    else { c_AUA = AUA / aua_barrier; }
+    return (c_TF + c_AUA) / 2;
+}
+var calc_link_weight = function (MF, FD) {
+    var c_MF, c_FD;
+    if (MF > mf_barrier) { c_MF = 1; }
+    else {c_MF = MF / mf_barrier;}
+    if (FD > fd_barrier) { c_FD = 1; }
+    else {c_FD = FD / fd_barrier; }
+    return (c_MF + c_FD) / 2;
+}
 
-    var findNode = function (Id) {
-        for (var i in graph["nodes"]) if (graph["nodes"][i]["Id"] === Id) return graph["nodes"][i];
-    }
-
-    var findLink = function (source, target){
-        for (var i in graph["links"]){
-            if (graph["links"][i]["source"]["Id"] === source && graph["links"][i]["target"]["Id"] === target)
-                return graph["links"][i];
-        }
-    }
-
-    var calc_link_weight = function (MF, FD){
-        var c_MF;
-        if (MF > mf_barrier){
-            c_MF = 1;
-        }else{
-            c_MF = MF/mf_barrier;
-        }
-        var c_FD;
-        if (FD > fd_barrier){
-            c_FD = 1;
-        }else{
-            c_FD = FD/fd_barrier;
-        }
-
-        return (c_MF + c_FD)/2;
-    }
-
-    var get_first_circle_link_weight = function (source_as_target){
-        return findLink(0, source_as_target)['Weight'];
-    }
-
-    this.addLink = function (source, i) {
-        var target_node = findNode(id_count);
-        var link_weight = calc_link_weight(target_node['MF'][i], target_node['FD'][i]);
-        var source_node = findNode(source);
-        graph["links"].push({"source":source_node,"target":target_node,
-                            "MF":target_node['MF'][i], "FD":target_node['FD'][i], "Weight":link_weight});
-
-        if (source > 0){
-            first_circle_link_weight = get_first_circle_link_weight(source);
-            tsp_to_add = target_node['Weight']*link_weight*source_node['Weight']*first_circle_link_weight;
-            if (tsp_to_add > target_node['TSP'])
-                target_node['TSP'] = tsp_to_add;
-        }
-        update();
-    }
-
-    this.increase_id_count = function () {
-        id_count++;
-    }
-
-    // set up the D3 visualisation in the specified element
-    var w = $(el).innerWidth(),
-        h = $(el).innerHeight();
-
-    var vis = d3.select(el).append("svg:svg")
-        .attr("width", w)
-        .attr("height", h);
-
-    var linkG = vis.append("g"),
-        linkTextG = vis.append("g"),
-        nodesG = vis.append("g");
-
-    var force = d3.layout.force()
-        .nodes(graph.nodes)
-        .links(graph.links)
-        .gravity(0.05)
-        .distance(200)
-        .charge(-100)
-        .size([w, h]);
-
-    var update = function () {
-
-        var link = linkG.selectAll("line.link")
-            .data(graph.links);
-
-        link.enter().insert("line")
-            .attr("class", "link")
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
-
-        link.exit().remove();
-
-        var linkText = linkTextG.selectAll("text.link-label").data(graph.links);
-
-        linkText.enter().insert("text")
-            .attr("class", "link-label")
-        .attr("font-family", "Arial, Helvetica, sans-serif")
-        .attr("fill", "Black")
-        .style("font", "normal 12px Arial")
-        .style("display", "blocked")
-        .attr("dy", ".35em")
-        .attr("text-anchor", "middle")
-        .text(function(d) {
-            return "MF="+d["MF"]+",FD="+d["FD"];
-        });
-
-
-
-        linkText.exit().remove();
-
-        var node = nodesG.selectAll("g.node")
-            .data(graph.nodes);
-
-        node.enter().append("g")
-            .attr("class", "node")
-            .call(force.drag);
-
-        node.append("circle")
-            .style("fill", function (d) { if (d.Id ==0){return "#0099ff"}
-                                          if (d.CF.includes('0')){return "#00cc00"}
-                                          if (d.TSP > 0.5){return "#ff9900"} else{return "#ff0000"}
-                                                    })
-            .attr("r", 5);
-
-        node.append("text")
-            .attr("class", "nodetext")
-            .attr("x", "0em")
-            .attr("y", 15)
-            .text(function(d) { return d.name });
-
-        node.on("mouseover", function(d) {
-          var g = d3.select(this); // The node
-          // The class is used to remove the additional text later
-          var info = g.append('text')
-             .classed('info', true)
-             .attr('dx', "0em")
-             .attr('dy', -10)
-             .text(function(d) { if(d.Id ==0){return "id=0"}
-                                 else{return "id="+d.Id.toString()+",TF="+d.TF.toString()+",AUA="+d.AUA.toString()}})
-             .style("font-size", "12px");
-        }).on("mouseout", function() {
-              // Remove the info text on mouse out.
-              d3.select(this).select('text.info').remove()
-            });
-
-        node.exit().remove();
-
-
-
-        force.on("tick", function() {
-          link.attr("x1", function(d) { return d.source.x; })
-              .attr("y1", function(d) { return d.source.y; })
-              .attr("x2", function(d) { return d.target.x; })
-              .attr("y2", function(d) { return d.target.y; });
-
-          linkText
-            .attr("x", function(d) {
-                return ((d.source.x + d.target.x)/2);
-            })
-            .attr("y", function(d) {
-                return ((d.source.y + d.target.y)/2);
-            });
-
-          node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-        });
-
-
-        // Restart the force layout.
-        force
-          .nodes(graph.nodes)
-          .links(graph.links)
-          .start();
-
-//        var q = document.querySelector("[linkTextTitle]");
-//        if (q){
-//            q.addEventListener("mouseover", function( event ) {
-//                // highlight the mouseover target
-//                event.target.style.color = "orange";
-//
-//            })
-//        }
-    }
-
-    // Make it all go
+// add find remove of nodes and links in DS
+function removeNode(name) {
+    nodes = _.filter(nodes, function (node) { return (node["name"] != name) });
+    links = _.filter(links, function (link) { return ((link["source"]["name"] != name) && (link["target"]["name"] != name)) });
     update();
+}
 
+function findNode(Id) {
+    for (var i in nodes) if (nodes[i]["id"] === Id) return nodes[i];
+}
+
+function findLink(source, target) {
+    for (var i in links) {
+        if (links[i]["source"]["id"] === source && links[i]["target"]["id"] === target)
+            return links[i];
+    }
+}
+
+function calculateLevel(CF) {
+    if (CF.includes('0')) { return 1; }
+    else { return 2; }
+}
+
+function addNode(Name, TF, AUA, CF, MF, FD) {
+    var node_weight = calc_node_weight(TF, AUA),
+        lvl = calculateLevel(CF);
+ 
+    nodes.push({
+        "id": id_count, "name": Name, "TF": TF, "AUA": AUA, "CF": CF, "MF": MF, "FD": FD,
+        "Weight": node_weight, "TSP": -1, 'level': lvl
+    });
 
 }
 
+var get_first_circle_link_weight = function (source_as_target) {
+    return findLink(0, source_as_target)['Weight'];
+}
 
-graph = new myGraph("#my_dataviz");
+function addLink(source, i) {
+    var target_node = findNode(id_count);
+    var link_weight = calc_link_weight(target_node['MF'][i], target_node['FD'][i]);
+    var source_node = findNode(source);
+    links.push({
+        "source": source_node, "target": target_node,
+        "MF": target_node['MF'][i], "FD": target_node['FD'][i], "Weight": link_weight
+    });
+
+    if (source > 0) {
+        first_circle_link_weight = get_first_circle_link_weight(source);
+        tsp_to_add = target_node['Weight'] * link_weight * source_node['Weight'] * first_circle_link_weight;
+        if (tsp_to_add > target_node['TSP'])
+            target_node['TSP'] = tsp_to_add;
+    }
+}
+
+function update() {
+    var link = d3.select('.links')
+        .selectAll('line.link')
+        .data(links).enter().insert("line")
+        .attr("class", "link")
+        .attr("x1", function (d) { return d.source.x; })
+        .attr("y1", function (d) { return d.source.y; })
+        .attr("x2", function (d) { return d.target.x; })
+        .attr("y2", function (d) { return d.target.y; });
+
+    link.exit().remove();
+
+    node = d3.select('.nodes')
+        .selectAll('g.node')
+        .data(nodes).enter().append("g")
+        .attr("class", "node");
+
+    node.append("circle")
+        .style("fill", function (d) {
+            if (d.id == 0) { return "#0099ff" }
+            if (d.CF.includes('0')) { return "#00cc00" }
+            if (d.TSP > 0.5) { return "#ff9900" } else { return "#ff0000" }
+        })
+        .attr("r", 5);
+
+    node.append("text")
+        .attr("class", "nodetext")
+        .attr("x", "0em")
+        .attr("y", 15)
+        .text(function (d) { return d.name });
+
+    node.on("mouseover", function (d) {
+        var g = d3.select(this); // The node
+        // The class is used to remove the additional text later
+        var info = g.append('text')
+            .classed('info', true)
+            .attr('dx', "0em")
+            .attr('dy', -10)
+            .text(function (d) {
+                if (d.id == 0) { return "id=0" }
+                else { return "id=" + d.Id.toString() + ",TF=" + d.TF.toString() + ",AUA=" + d.AUA.toString() }
+            })
+            .style("font-size", "12px");
+    }).on("mouseout", function () {
+        // Remove the info text on mouse out.
+        d3.select(this).select('text.info').remove()
+    });
 
 
+    node.exit().remove();
 
-// These are the sort of commands I want to be able to give the object.
-function addNode() {
-    var name = document.getElementById("NodeName").value
-    var tf = document.getElementById("TF").value
-    var aua = document.getElementById("AUA").value
-    var cf = document.getElementById("CF").value.split(",")
-    var mf = document.getElementById("MF").value.split(",")
-    var fd = document.getElementById("FD").value.split(",")
+    drag_handler(node);
+    simulation.nodes(nodes);
+    simulation.force("link").links(links);
+    simulation.alpha(1).restart();
+}
 
-    graph.addNode(name, tf, aua, cf, mf, fd);
+
+function ticked() {
+    var link = d3.select('.links')
+        .selectAll('line.link')
+        .data(links);
+    link.attr("x1", function (d) { return d.source.x; })
+        .attr("y1", function (d) { return d.source.y; })
+        .attr("x2", function (d) { return d.target.x; })
+        .attr("y2", function (d) { return d.target.y; });
+
+
+    node = d3.select('.nodes')
+        .selectAll('g.node')
+        .data(nodes);
+
+    node.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
+}
+
+update();
+
+
+function addNodeToGraph() {
+    var name = document.getElementById("NodeName").value,
+        tf = document.getElementById("TF").value,
+        aua = document.getElementById("AUA").value,
+        cf = document.getElementById("CF").value.split(","),
+        mf = document.getElementById("MF").value.split(","),
+        fd = document.getElementById("FD").value.split(",");
+
+    addNode(name, tf, aua, cf, mf, fd);
     var i;
     for (i = 0; i < cf.length; i++) {
-        graph.addLink(parseInt(cf[i]), i);
+        addLink(parseInt(cf[i]), i);
     }
-    graph.increase_id_count();
+    increase_id_count();
+    console.log(nodes);
+    console.log(links);
+    update();
 }
---
+++ FlaskTry/static/js/graphPresentationOld.js
@@ -0,0 +1,256 @@
+function myGraph(el) {
+    var id_count = 1;
+    var tf_barrier = 100;
+    var aua_barrier = 365;
+    var mf_barrier = 20;
+    var fd_barrier = 12;
+    var msp = 0.5;
+
+    // Initialise the graph object
+    var graph = this.graph = {
+        "nodes": [{ "Id": 0, "name": "Ego Node", "TF": "", "AUA": "", "CF": [], "MF": [], "FD": [], "Weight": -1, "TSP": -1 }],
+        "links": []
+    };
+
+    var calc_node_weight = function (TF, AUA) {
+        var c_TF;
+        if (TF > tf_barrier) {
+            c_TF = 1;
+        } else {
+            c_TF = TF / tf_barrier;
+        }
+        var c_AUA;
+        if (AUA > aua_barrier) {
+            c_AUA = 1;
+        } else {
+            c_AUA = AUA / aua_barrier;
+        }
+
+        return (c_TF + c_AUA) / 2;
+    }
+
+    // Add and remove elements on the graph object
+    this.addNode = function (Name, TF, AUA, CF, MF, FD) {
+        var node_weight = calc_node_weight(TF, AUA);
+        graph["nodes"].push({
+            "Id": id_count, "name": Name, "TF": TF, "AUA": AUA, "CF": CF, "MF": MF, "FD": FD,
+            "Weight": node_weight, "TSP": -1
+        });
+
+    }
+
+    this.removeNode = function (name) {
+        graph["nodes"] = _.filter(graph["nodes"], function (node) { return (node["name"] != name) });
+        graph["links"] = _.filter(graph["links"], function (link) { return ((link["source"]["name"] != name) && (link["target"]["name"] != name)) });
+        update();
+    }
+
+    var findNode = function (Id) {
+        for (var i in graph["nodes"]) if (graph["nodes"][i]["Id"] === Id) return graph["nodes"][i];
+    }
+
+    var findLink = function (source, target) {
+        for (var i in graph["links"]) {
+            if (graph["links"][i]["source"]["Id"] === source && graph["links"][i]["target"]["Id"] === target)
+                return graph["links"][i];
+        }
+    }
+
+    var calc_link_weight = function (MF, FD) {
+        var c_MF;
+        if (MF > mf_barrier) {
+            c_MF = 1;
+        } else {
+            c_MF = MF / mf_barrier;
+        }
+        var c_FD;
+        if (FD > fd_barrier) {
+            c_FD = 1;
+        } else {
+            c_FD = FD / fd_barrier;
+        }
+
+        return (c_MF + c_FD) / 2;
+    }
+
+    var get_first_circle_link_weight = function (source_as_target) {
+        return findLink(0, source_as_target)['Weight'];
+    }
+
+    this.addLink = function (source, i) {
+        var target_node = findNode(id_count);
+        var link_weight = calc_link_weight(target_node['MF'][i], target_node['FD'][i]);
+        var source_node = findNode(source);
+        graph["links"].push({
+            "source": source_node, "target": target_node,
+            "MF": target_node['MF'][i], "FD": target_node['FD'][i], "Weight": link_weight
+        });
+
+        if (source > 0) {
+            first_circle_link_weight = get_first_circle_link_weight(source);
+            tsp_to_add = target_node['Weight'] * link_weight * source_node['Weight'] * first_circle_link_weight;
+            if (tsp_to_add > target_node['TSP'])
+                target_node['TSP'] = tsp_to_add;
+        }
+        update();
+    }
+
+    this.increase_id_count = function () {
+        id_count++;
+    }
+
+    // set up the D3 visualisation in the specified element
+    var w = $(el).innerWidth(),
+        h = $(el).innerHeight();
+
+    var vis = d3.select(el).append("svg:svg")
+        .attr("width", w)
+        .attr("height", h);
+
+    var linkG = vis.append("g"),
+        linkTextG = vis.append("g"),
+        nodesG = vis.append("g");
+
+    var force = d3.layout.force()
+        .nodes(graph.nodes)
+        .links(graph.links)
+        .gravity(0.05)
+        .distance(200)
+        .charge(-100)
+        .size([w, h]);
+
+    var update = function () {
+
+        var link = linkG.selectAll("line.link")
+            .data(graph.links);
+
+        link.enter().insert("line")
+            .attr("class", "link")
+            .attr("x1", function (d) { return d.source.x; })
+            .attr("y1", function (d) { return d.source.y; })
+            .attr("x2", function (d) { return d.target.x; })
+            .attr("y2", function (d) { return d.target.y; });
+
+        link.exit().remove();
+
+        var linkText = linkTextG.selectAll("text.link-label").data(graph.links);
+
+        linkText.enter().insert("text")
+            .attr("class", "link-label")
+            .attr("font-family", "Arial, Helvetica, sans-serif")
+            .attr("fill", "Black")
+            .style("font", "normal 12px Arial")
+            .style("display", "blocked")
+            .attr("dy", ".35em")
+            .attr("text-anchor", "middle")
+            .text(function (d) {
+                return "MF=" + d["MF"] + ",FD=" + d["FD"];
+            });
+
+
+
+        linkText.exit().remove();
+
+        var node = nodesG.selectAll("g.node")
+            .data(graph.nodes);
+
+        node.enter().append("g")
+            .attr("class", "node")
+            .call(force.drag);
+
+        node.append("circle")
+            .style("fill", function (d) {
+                if (d.Id == 0) { return "#0099ff" }
+                if (d.CF.includes('0')) { return "#00cc00" }
+                if (d.TSP > 0.5) { return "#ff9900" } else { return "#ff0000" }
+            })
+            .attr("r", 5);
+
+        node.append("text")
+            .attr("class", "nodetext")
+            .attr("x", "0em")
+            .attr("y", 15)
+            .text(function (d) { return d.name });
+
+        node.on("mouseover", function (d) {
+            var g = d3.select(this); // The node
+            // The class is used to remove the additional text later
+            var info = g.append('text')
+                .classed('info', true)
+                .attr('dx', "0em")
+                .attr('dy', -10)
+                .text(function (d) {
+                    if (d.Id == 0) { return "id=0" }
+                    else { return "id=" + d.Id.toString() + ",TF=" + d.TF.toString() + ",AUA=" + d.AUA.toString() }
+                })
+                .style("font-size", "12px");
+        }).on("mouseout", function () {
+            // Remove the info text on mouse out.
+            d3.select(this).select('text.info').remove()
+        });
+
+        node.exit().remove();
+
+
+
+        force.on("tick", function () {
+            link.attr("x1", function (d) { return d.source.x; })
+                .attr("y1", function (d) { return d.source.y; })
+                .attr("x2", function (d) { return d.target.x; })
+                .attr("y2", function (d) { return d.target.y; });
+
+            linkText
+                .attr("x", function (d) {
+                    return ((d.source.x + d.target.x) / 2);
+                })
+                .attr("y", function (d) {
+                    return ((d.source.y + d.target.y) / 2);
+                });
+
+            node.attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
+        });
+
+
+        // Restart the force layout.
+        force
+            .nodes(graph.nodes)
+            .links(graph.links)
+            .start();
+
+        //        var q = document.querySelector("[linkTextTitle]");
+        //        if (q){
+        //            q.addEventListener("mouseover", function( event ) {
+        //                // highlight the mouseover target
+        //                event.target.style.color = "orange";
+        //
+        //            })
+        //        }
+    }
+
+    // Make it all go
+    update();
+
+
+}
+
+
+graph = new myGraph("#my_dataviz");
+
+
+
+// These are the sort of commands I want to be able to give the object.
+function addNode() {
+    var name = document.getElementById("NodeName").value,
+        tf = document.getElementById("TF").value,
+        aua = document.getElementById("AUA").value,
+        cf = document.getElementById("CF").value.split(","),
+        mf = document.getElementById("MF").value.split(","),
+        fd = document.getElementById("FD").value.split(",");
+
+    addNode(name, tf, aua, cf, mf, fd);
+    var i;
+    for (i = 0; i < cf.length; i++) {
+        addLink(parseInt(cf[i]), i);
+    }
+    increase_id_count();
+}

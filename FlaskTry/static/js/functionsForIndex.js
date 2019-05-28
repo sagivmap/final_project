42dc39d5294646c4ab8db292f78846e17cfad4da
DESKTOP-USEP09U\sagiv <sagivmap@gmail.com>
2019-05-28 10:06:13 +0300

    logger for crawling

--- FlaskTry/static/js/functionsForIndex.js
+++ FlaskTry/static/js/functionsForIndex.js
@@ -1,5 +1,34 @@
 var modal = document.getElementById("myModal");
+var showLoggerBtn = document.getElementById("showlogger");
+var crawlFacebookBtn = document.getElementById("crawlFBBtn");
+var span = document.getElementById("closeFBLogger");
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
 
 function showFloatingWindow() {
     modal.style.display = "block";
+    showLoggerBtn.style.display = "block";
+    crawlFacebookBtn.style.display = "none";
 }
+
+
+function showLogger() {
+    modal.style.display = "block";
+}
+
+
+var scrolled = false;
+function updateScroll() {
+    if (!scrolled) {
+        var element = document.getElementById("output");
+        element.scrollTop = element.scrollHeight;
+    }
+}
+
+setInterval(updateScroll, 1000);
+
+$("#output").on('scroll', function () {
+    scrolled = true;
+});

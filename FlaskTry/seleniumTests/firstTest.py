9486b920db798bf647ae8f56c2e13a0dc9bcdaa9
OMER SELLA <omersella14@gmail.com>
2019-06-16 18:39:30 +0300

    geckodriver

--- FlaskTry/seleniumTests/firstTest.py
+++ FlaskTry/seleniumTests/firstTest.py
@@ -1,11 +1,10 @@
 import unittest
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+from time import sleep
 
-class TestLoginToFacebook(unittest.TestCase):
+from selenium import webdriver
+import os
+
+class TestAddNewNodes(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -14,13 +13,35 @@ class TestLoginToFacebook(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_valid_cradentials_from_config_file(self):
+    def test_ego_node(self):
         element = self.browser.find_element_by_id('MoveToManuallyAddPage')
         self.browser.execute_script("arguments[0].click();", element)
-        self.assertEqual(1,1)
+        sleep(5)
+        nodes = self.browser.execute_script("return nodes;")
+        self.assertEqual(len(nodes),1)
+        self.assertEqual(nodes[0]['name'], 'Ego Node')
+        self.assertEqual(nodes[0]['id'], 0)
+        self.assertEqual(nodes[0]['AUA'], "")
+        self.assertEqual(len(nodes[0]['CF']),0)
+        self.assertEqual(len(nodes[0]['FD']),0)
+        self.assertEqual(len(nodes[0]['MF']),0)
+        self.assertEqual(nodes[0]['TF'], "")
+        self.assertEqual(nodes[0]["level"],0)
+        self.assertEqual(nodes[0]["TSP"],-1)
+        self.assertEqual(nodes[0]["Weight"],-1)
 
-
-
+    #This test needs a export.json file in the seleniumTests directory
+    def test_upload_csv(self):
+        element = self.browser.find_element_by_id('MoveToManuallyAddPage')
+        self.browser.execute_script("arguments[0].click();", element)
+        sleep(5)
+        uploadFile=(os.path.dirname(os.path.abspath(__file__))+"\\files\\export.json")
+        selectFiles=self.browser.find_element_by_id('selectFiles').send_keys(uploadFile)
+        uploadCSVfile=self.browser.find_element_by_id('import')
+        self.browser.execute_script("arguments[0].click();", uploadCSVfile)
+        sleep(5)
+        nodes = self.browser.execute_script("return nodes;")
+        self.assertNotEqual(len(nodes),1)
 
 if __name__ == '__main__':
     unittest.main()
